0d6b9598c3df5c302c30e359266c8bda3a21bad9
WisdomFusion <WisdomFusion@gmail.com>
2013-08-14 21:16:24 +0800

    add auctex

--- init.el
+++ init.el
@@ -1,7 +1,10 @@
 ;;; -*- mode: emacs-Lisp; coding: utf-8 -*-
 
+;; require packages
 (require 'org-install)
 (require 'ob-tangle)
+
+;; load config.org
 (org-babel-load-file "~/.emacs.d/config.org")
 
 ;;; init.el ends here
