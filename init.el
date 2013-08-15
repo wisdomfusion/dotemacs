cdfd288ac753755f99fa8c0771f558500b342093
WisdomFusion <WisdomFusion@gmail.com>
2013-08-15 21:20:29 +0800

    add switch

--- init.el
+++ init.el
@@ -4,7 +4,11 @@
 (require 'org-install)
 (require 'ob-tangle)
 
+;; switch
+(defconst *hack-slime-p* nil)             ; turn off before slime working
+
 ;; load config.org
 (org-babel-load-file "~/.emacs.d/config.org")
 
 ;;; init.el ends here
+
