eb3cb1f5494b71116b05434a2e7ed4254aeb4382
WisdomFusion <WisdomFusion@gmail.com>
2016-01-07 14:06:37 +0800

    fix lisp with slime

--- init.el
+++ init.el
@@ -4,8 +4,10 @@
 (require 'org-install)
 (require 'ob-tangle)
 
-;; switch
-(defconst *hack-slime-p* nil)             ; turn off before slime working
+;; (setenv "PATH" (concat (getenv "PATH") ":/opt/local/bin"))
+
+;; turn on slime when lisp and slime working
+(defconst *hack-slime-p* 1)
 
 ;; load config.org
 (org-babel-load-file "~/.emacs.d/config.org")
