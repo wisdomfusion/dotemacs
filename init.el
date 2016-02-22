2f5c8e6d66d9e5150abffec1a53c9638f3014b59
WisdomFusion <WisdomFusion@gmail.com>
2016-02-22 10:27:58 +0800

    config AUCTeX

--- init.el
+++ init.el
@@ -4,18 +4,6 @@
 (require 'org-install)
 (require 'ob-tangle)
 
-;; PATH env and exec-path
-;; difference between exec-path and PATH.
-;; The value of environment variable “PATH” is used by emacs
-;; when you are running a shell in emacs, similar to when you
-;; are using a shell in a terminal.
-;; The exec-path is used by emacs itself to find programs it
-;; needs for its features, such as spell checking, file
-;; compression, compiling, grep, diff, etc. Original from
-;; http://ergoemacs.org/emacs/emacs_env_var_paths.html
-(setenv "PATH" (concat (getenv "PATH") ":/opt/local/bin"))
-(setq exec-path (append exec-path '("/opt/local/bin")))
-
 ;; turn on slime when lisp and slime working
 (defconst *hack-slime-p* 1)
 
