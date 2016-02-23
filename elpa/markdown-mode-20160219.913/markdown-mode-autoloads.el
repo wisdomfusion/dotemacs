b6120dcfd4a3372ef6d8668d293633d58d5fa332
WisdomFusion <WisdomFusion@gmail.com>
2016-02-23 09:05:56 +0800

    upgrade some packages

--- elpa/markdown-mode-20160219.913/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20160219.913/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22153 2799
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22218 45205
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
