bf9d12baa48a356588d596030b2718cdd0daef73
WisdomFusion <WisdomFusion@gmail.com>
2016-06-17 09:40:49 +0800

    remove tabbar, upgrade packages

---
+++ elpa/chinese-pyim-20160615.1958/chinese-pyim-pkg.el
@@ -0,0 +1,10 @@
+(define-package "chinese-pyim" "20160615.1958" "Chinese pinyin input method"
+  '((cl-lib "0.5")
+    (pos-tip "0.4")
+    (popup "0.1")
+    (async "1.6"))
+  :url "https://github.com/tumashu/chinese-pyim" :keywords
+  '("convenience" "chinese" "pinyin" "input-method"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
