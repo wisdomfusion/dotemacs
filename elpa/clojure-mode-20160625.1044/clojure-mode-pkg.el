b2f1f54d05b46f8e534f5f93cc4079279dbe3a59
WisdomFusion <WisdomFusion@gmail.com>
2016-06-26 08:52:26 +0800

    upgrade packages

---
+++ elpa/clojure-mode-20160625.1044/clojure-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojure-mode" "20160625.1044" "Major mode for Clojure code" '((emacs "24.3")) :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "clojure" "clojurescript" "lisp"))
