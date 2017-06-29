8d1c4ff33dee90dab13e505611c678e80a6280e1
WisdomFusion <WisdomFusion@gmail.com>
2017-06-29 08:41:21 +0800

    enable helm by default

---
+++ elpa/ac-helm-2.1/ac-helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ac-helm" "2.1" "Auto Complete with Helm" '((helm "20130328") (auto-complete "1.4.0") (popup "0.5.0")) :commit "f2110576b0eb35850a7f638c1a991a9fa0c8da3a" :keywords '("completion" "convenience" "helm"))
