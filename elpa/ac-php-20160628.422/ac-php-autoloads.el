a69e09e81436709345310fb6e2bbebc7406007b3
WisdomFusion <WisdomFusion@gmail.com>
2017-08-01 09:49:43 +0800

    重新整理 elpa

--- elpa/ac-php-20160628.422/ac-php-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; ac-php-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("ac-php.el") (22397 6073 763347 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ac-php-autoloads.el ends here
--- elpa/flycheck-perl6-20150414.1832/flycheck-perl6-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; flycheck-perl6-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("flycheck-perl6.el") (22152 35165 623000
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flycheck-perl6-autoloads.el ends here
