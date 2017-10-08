5c99cfcadb0520549b4dab5be5c0082559fb4790
WisdomFusion <WisdomFusion@gmail.com>
2017-10-08 10:24:58 +0800

    upgrade

--- elpa/helm-smex-0.3/helm-smex-autoloads.el
+++ elpa/helm-smex-0.3/helm-smex-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "helm-smex" "helm-smex.el" (22992 58336 0 0))
+;;;### (autoloads nil "helm-smex" "helm-smex.el" (23001 35862 0 0))
 ;;; Generated autoloads from helm-smex.el
 
 (autoload 'helm-smex "helm-smex" "\
