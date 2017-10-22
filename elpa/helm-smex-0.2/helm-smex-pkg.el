fd54b2255bfc9681af4f0a292b7f30f744ac322d
WisdomFusion <WisdomFusion@gmail.com>
2017-10-22 21:22:42 +0800

    upgrade

--- elpa/helm-smex-0.2/helm-smex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-smex" "0.2" "Helm interface for smex" '((emacs "24") (smex "3.0") (helm "1.7.7")) :commit "7af4e4b44671f739b39584fc50c20084700701ac" :keywords '("convenience"))
